50564fa4a90dde92d4aa82fa631f550f1b877938
ritabobitaritabobita <rrosiek97@gmail.com>
2025-01-19 17:44:58 -0500

    create featured product grid component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import VideoHero from "./components/Video Hero/VideoHero";
+import FeaturedProductGrid from "./components/Featured Product Grid/FeaturedProductGrid";
 
 export default function Home() {
   return (
-    // tailwind was in className previously
     <main>
       <VideoHero />
+      <h2 className="text-center text-6xl font-bold my-24">Our Products</h2>
+      <FeaturedProductGrid />
     </main>
   );
 }
