9a0c811ce0233f6fc88cf1f13b5c278c6870c1ca
ritabobitaritabobita <rrosiek97@gmail.com>
2025-02-12 18:26:52 -0500

    create dynamic products page

---
+++ src/app/products/[id].js
@@ -0,0 +1,26 @@
+import { useRouter } from 'next/router';
+import styles from './ProductPage.module.scss';
+
+const ProductPage = () => {
+
+    const router = useRouter();
+    const { id } = router.query;
+
+    const product = {
+        name: "Bud Vase",
+        price: "$100",
+        description: "A beautiful bud vase",
+        image: "https://i.etsystatic.com/35122230/c/2312/1838/79/148/il/b86066/4373628315/il_680x540.4373628315_7lkq.jpg"
+    }
+
+    return (
+        <div className={`${styles.ProductPage}`}>
+            <h1>{product.name}</h1>
+            <p>{product.price}</p>
+            <p>{product.description}</p>
+            <img src={product.image} alt={product.name} />
+        </div>
+    )
+}
+
+export default ProductPage;
