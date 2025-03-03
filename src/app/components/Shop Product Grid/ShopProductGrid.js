a5c33138a530fef8635b9125b55c472c15308926
ritabobitaritabobita <rrosiek97@gmail.com>
2025-03-03 16:07:51 -0500

    implement product grid to other pages

--- src/app/components/Shop Product Grid/ShopProductGrid.js
+++ src/app/components/Shop Product Grid/ShopProductGrid.js
@@ -1,93 +1,55 @@
 'use client'
+import { useEffect, useState } from 'react';
+import { formattedNumber } from '../../utils/utils';
 import styles from './ShopProductGrid.module.scss';
 import ProductButton from '../Product Button/ProductButton';
+import Link from 'next/link';
 
 export default function ShopProductGrid() {
+    const [products, setProducts] = useState([]);
+    const [prices, setPrices] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await fetch('/api/products');
+            const data = await response.json();
+            console.log(data)
+            setProducts(data);
+        };
+
+        const fetchPrices = async () => {
+            const response = await fetch('/api/prices');
+            const data = await response.json();
+            console.log(data)
+            setPrices(data);
+        };
+
+        fetchProducts();
+        fetchPrices();
+    }, []);
+
     return (
         <div className={styles.shopProductGrid}>
             <section className={styles.products}>
-            <article className={styles.productCard}>
-                <img
-                    src="https://i.etsystatic.com/35122230/c/2312/1838/79/148/il/b86066/4373628315/il_680x540.4373628315_7lkq.jpg"
-                    alt="Bud Vase"
-                />
-                <div className={styles.productInfo}>
-                    <h3 className={styles.productName}>Bud Vase</h3>
-                    <p className={styles.productPrice}>$100</p>
-                </div>
-            </article>
-            <article className={styles.productCard}>
-                <img
-                    src="https://i.etsystatic.com/35122230/c/2910/2237/0/0/il/226620/4715730938/il_680x540.4715730938_bpmd.jpg"
-                    alt="Bud Vase"
-                />
-                <div className={styles.productInfo}>
-                    <h3 className={styles.productName}>Bud Vase</h3>
-                    <p className={styles.productPrice}>$100</p>
-                </div>
-            </article>
-            <article className={styles.productCard}>
-                <img
-                    src="https://i.etsystatic.com/35122230/c/3000/2384/0/0/il/df540c/4087687814/il_680x540.4087687814_1y1j.jpg"
-                    alt="Flower Vase"
-                />
-                <div className={styles.productInfo}>
-                    <h3 className={styles.productName}>Flower Vase</h3>
-                    <p className={styles.productPrice}>$100</p>
-                </div>
-            </article>
-            <article className={styles.productCard}>
-                <img 
-                    src="https://i.etsystatic.com/35122230/r/il/abbddb/4715682318/il_340x270.4715682318_5tu8.jpg"
-                    alt="Creamer"
-                />
-                <div className={styles.productInfo}>
-                    <h3 className={styles.productName}>Creamer</h3>
-                    <p className={styles.productPrice}>$100</p>
-                </div>
-            </article>
-            <article className={styles.productCard}>
-                <img
-                    src="https://i.etsystatic.com/35122230/c/2445/1943/553/41/il/d8ba31/4699605164/il_340x270.4699605164_p4po.jpg"
-                    alt="Bud Vase"
-                />
-                <div className={styles.productInfo}>
-                    <h3 className={styles.productName}>Bud Vase</h3>
-                    <p className={styles.productPrice}>$100</p>
-                </div>
-            </article>
-            <article className={styles.productCard}>
-                <img
-                    src="https://i.etsystatic.com/35122230/c/2922/2053/0/0/il/423eec/4763865791/il_340x270.4763865791_edc7.jpg"
-                    alt="Bud Vase"
-                />
-                <div className={styles.productInfo}>
-                    <h3 className={styles.productName}>Bud Vase</h3>
-                    <p className={styles.productPrice}>$100</p>
-                </div>
-            </article>
-            <article className={styles.productCard}>
-                <img
-                    src="https://i.etsystatic.com/35122230/c/2922/2053/0/0/il/423eec/4763865791/il_340x270.4763865791_edc7.jpg"
-                    alt="Bud Vase"
-                />
-                <div className={styles.productInfo}>
-                    <h3 className={styles.productName}>Bud Vase</h3>
-                    <p className={styles.productPrice}>$100</p>
-                </div>
-            </article>
-            <article className={styles.productCard}>
-                <img
-                    src="https://i.etsystatic.com/35122230/c/2922/2053/0/0/il/423eec/4763865791/il_340x270.4763865791_edc7.jpg"
-                    alt="Bud Vase"
-                />
-                <div className={styles.productInfo}>
-                    <h3 className={styles.productName}>Bud Vase</h3>
-                    <p className={styles.productPrice}>$100</p>
-                </div>
-            </article>
+
+                {products.map(product => {
+                    const productPrice = prices.find(price => price.id === product.default_price) ?.unit_amount || 'Price not available';
+
+                    return (
+                    <article key={product.id} className={styles.productCard}>
+                        <Link href={`/product/${product.id}`}>
+                        <img src={product.images[0]} alt={product.name}
+                        />
+                        <div className={styles.productInfo}>
+                            <h3 className={styles.productName}>{product.name}</h3>
+                            <p className={styles.productPrice}>{formattedNumber(productPrice)}</p>
+                        </div>
+                        </Link>
+                    </article>
+                    );
+                })}
         </section>
-        {/* <ProductButton className={styles.viewAllProducts} href="/shop">View All Products</ProductButton> */}
+        <ProductButton className={styles.viewAllProducts} href="/shop">View All Products</ProductButton>
         </div>
     )
 }
