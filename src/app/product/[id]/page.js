6d437b7ff4aa5ef14b3b2b2910350cc5a86d2b73
ritabobitaritabobita <rrosiek97@gmail.com>
2025-03-03 21:15:08 -0500

    fetch prices for product page

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -7,8 +7,9 @@ const ProductPage = () => {
     // const router = useRouter();
     const pathname = usePathname();
     const id = pathname.split('/').pop();
-    console.log("Product ID:", id);
+    // console.log("Product ID:", id);
     const [product, setProduct] = useState(null);
+    const [prices, setPrices] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
@@ -21,6 +22,7 @@ const ProductPage = () => {
                         throw new Error('Product not found');
                     }
                     const data = await response.json();
+                    console.log(data)
                     setProduct(data);
                 } catch (err) {
                     console.error("Error fetching product:", err.message);
@@ -30,7 +32,15 @@ const ProductPage = () => {
                 }
             };
 
+            const fetchPrices = async () => {
+                const response = await fetch('/api/prices');
+                const data = await response.json();
+                console.log(data)
+                setPrices(data);
+            };
+
             fetchProduct();
+            fetchPrices();
         } else {
             setError("Invalid product ID");
             setLoading(false);
@@ -41,13 +51,16 @@ const ProductPage = () => {
     if (error) return <div>Error: {error}</div>;
     if (!product) return <div>No product found.</div>;
 
+    const productPrice = prices.find(price => price.id === product.default_price)?.unit_amount || 'Price not available';
+
+
     return (
-        <div key={product.id} className={`${styles.ProductPage}`}>
-            <h1>{product.name}</h1>
-            <p>{product.price}</p>
-            <p>{product.description}</p>
-            <img src={product.image} alt={product.name} />
-        </div>
+                <div key={product.id} className={`${styles.ProductPage}`}>
+                    <h1>{product.name}</h1>
+                    <p>{productPrice}</p>
+                    <p>{product.description}</p>
+                    <img src={product.images[0]} alt={product.name} />
+                </div>
     );
 };
 
