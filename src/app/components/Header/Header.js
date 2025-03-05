534da9ea82fc8a0f23f63473f10bac0258cc4f69
ritabobitaritabobita <rrosiek97@gmail.com>
2025-03-04 19:50:20 -0500

    allow menu toggle when clicking on links

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -7,25 +7,35 @@ import { useState } from 'react';
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [menuClick, setMenuClick] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
-    setIsMenuOpen(!isMenuOpen);
+
+    if (!isOpen) {
+      document.body.classList.add('overflow-hidden');
+    } else {
+      document.body.classList.remove('overflow-hidden');
+    }
   };
+
+  const handleMenuClick = () => {
+    setMenuClick(!menuClick);
+    toggleMenu();
+  }
 
   return (
     <header className={styles.header}>
       <nav className="flex items-center justify-between flex-wrap bg-transparent px-6">
       <div className={`${styles.logo} flex items-center flex-shrink-0 text-white mr-6`}>
-        <Link href="/">
+        <Link href="/" onClick={handleMenuClick}>
             <Image className="fill-current mr-2" src="/images/Ritka-Header.svg" alt="Ritka Studios Logo" width={100} height={100} />
-          </Link>
+        </Link>
         </div>
 
         {/* Menu Icon on tablet and mobile */}
         <div className={`${styles.hamburger}`}>
-        {isMenuOpen ? <X size={24} onClick={toggleMenu} /> : <Menu size={24} onClick={toggleMenu} />}
+        {isOpen ? <X size={24} onClick={toggleMenu} /> : <Menu size={24} onClick={toggleMenu} />}
         </div>
 
         {/* Menu on desktop */}
@@ -54,11 +64,11 @@ export default function Header() {
       {isOpen && (
           <div className={styles.mobileMenu}>
             <ul>
-              <li><Link href="/">Home</Link></li>
-              <li><Link href="/shop">Shop</Link></li>
-              <li><Link href="/seconds">Seconds</Link></li>
-              <li><Link href="/about">About</Link></li>
-              <li><Link href="/contact">Contact</Link></li>
+              <li><Link href="/" onClick={handleMenuClick}>Home</Link></li>
+              <li><Link href="/shop" onClick={handleMenuClick}>Shop</Link></li>
+              <li><Link href="/seconds" onClick={handleMenuClick}>Seconds</Link></li>
+              <li><Link href="/about" onClick={handleMenuClick}>About</Link></li>
+              <li><Link href="/contact" onClick={handleMenuClick}>Contact</Link></li>
             </ul>
           </div>
         )}
