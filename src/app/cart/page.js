3f54d25967510d5dfb8426cda1c6a3efac9ec24c
ritabobitaritabobita <rrosiek97@gmail.com>
2025-03-05 14:04:26 -0500

    cart page functionality set up

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,36 +1,29 @@
 'use client';
 import styles from './Cart.module.scss';
 import { useState } from 'react';
+import { LucideMinus, LucidePlus, X } from 'lucide-react';
 
 export default function Cart() {
-    // const [cartItems, setCartItems] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
 
-    // const addToCart = (item) => {
-    //     setCartItems([...cartItems, item]);
-    // }; 
+    const removeFromCart = (item) => {
+        setCartItems(cartItems.filter(i => i !== item));
+    };  
 
-    // const removeFromCart = (item) => {
-    //     setCartItems(cartItems.filter(i => i !== item));
-    // };  
+    const getTotal = () => {
+        return cartItems.reduce((total, item) => total + item.price, 0);
+    };
 
-    // const getTotal = () => {
-    //     return cartItems.reduce((total, item) => total + item.price, 0);
-    // };
+    const handleCheckout = () => {
+        console.log('Checkout button clicked');
+    };
 
-    // const clearCart = () => {
-    //     setCartItems([]);
-    // };
-
-    // const handleCheckout = () => {
-    //     console.log('Checkout button clicked');
-    // };
-
-    // const handleQuantityChange = (item, quantity) => {
-    //     const updatedItems = cartItems.map(i => 
-    //         i.id === item.id ? { ...i, quantity } : i
-    //     );
-    //     setCartItems(updatedItems);
-    // };
+    const handleQuantityChange = (item, quantity) => {
+        const updatedItems = cartItems.map(i => 
+            i.id === item.id ? { ...i, quantity } : i
+        );
+        setCartItems(updatedItems);
+    };
 
     return (
         <div className={`${styles.cart} p-4`}>
@@ -43,16 +36,16 @@ export default function Cart() {
                         <span>$10.00</span>
                     </div>
                 </div>
-                <div className="flex flex-row gap-5 items-center">
+                <div className="flex flex-row items-center gap-12">
                     <div className="flex flex-col items-center justify-center gap-3">
                         <span>Quantity</span>
                         <span>
-                            <button className={`${styles.quantityButton} border border-gray-400 text-black py-1 px-3 hover:bg-gray-200`}>
-                                -
+                            <button onClick={() => handleQuantityChange(item, item.quantity - 1)} className={`${styles.quantityButton} border border-gray-400 text-black py-2 px-2 hover:bg-gray-200`}>
+                                <LucideMinus className="w-3 h-3" />
                             </button>
                             <span className="mx-2">1</span>
-                            <button className={`${styles.quantityButton} border border-gray-400 text-black py-1 px-3 hover:bg-gray-200`}>
-                                +
+                            <button onClick={() => handleQuantityChange(item, item.quantity + 1)} className={`${styles.quantityButton} border border-gray-400 text-black py-2 px-2 hover:bg-gray-200`}>
+                                <LucidePlus className="w-3 h-3" />
                             </button>
                         </span>
                     </div>
@@ -60,11 +53,14 @@ export default function Cart() {
                         <span>Total</span>
                         <span>$10.00</span>
                     </div>
+                    <button onClick={() => removeFromCart(item)} className={`${styles.trashButton} py-1 px-3 hover:text-gray-500`}>
+                        <X className="w-6 h-6" />
+                    </button>
                 </div>
             </div>
             <div className={`${styles.totalContainer} mt-4 flex flex-col items-end`}>
-                <h3 className="text-xl">Subtotal: $25.00</h3>
-            <button className={`${styles.checkoutButton} mt-4 border border-black text-black py-2 px-4 hover:bg-gray-200`}>
+                <h3 className="text-xl">Subtotal: ${getTotal()}</h3>
+            <button onClick={handleCheckout} className={`${styles.checkoutButton} mt-4 border border-black text-black py-2 px-4 hover:bg-gray-200`}>
                 Proceed to Checkout
             </button>
             </div>
