11fa5c1a2dae2605dc40b3ecf1a6c1599cd66e25
ritabobitaritabobita <rrosiek97@gmail.com>
2025-08-09 13:39:36 -0400

    feature flag clean up and test implementation for shop page

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -1,10 +1,14 @@
-import ShopProductGrid from '../components/Shop Product Grid/ShopProductGrid';  
+import ShopProductGrid from '../components/Shop Product Grid/ShopProductGrid';
+import { showEcommerce } from '../../lib/flags';
 
-export default function Shop() {
+export default async function Shop() {
+
+  const ecommerceEnabled = await showEcommerce();
+
     return (
       <main>
         <h1 className="mt-9 text-4xl text-center font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl">Shop Ritka Studios</h1>
-        <ShopProductGrid />
+        {ecommerceEnabled ? <ShopProductGrid /> : <div>Ecommerce is disabled</div>}
       </main>
     );
   }
--- src/lib/flags.js
+++ src/lib/flags.js
@@ -1,4 +1,4 @@
-import { unstable_flag as flag } from "@vercel/flags/next";
+import { flag } from '@vercel/flags/next';
 
 export const showEcommerce = flag({
   key: "ecommerce-enabled",
