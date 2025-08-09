8c574ecb2aa987f0ad28979d8b582d14a54d71cb
ritabobitaritabobita <rrosiek97@gmail.com>
2025-08-09 14:15:14 -0400

    implement feature flag for product grid on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import VideoHero from "./components/Video Hero/VideoHero";
 import FeaturedProductGrid from "./components/Featured Product Grid/FeaturedProductGrid";
+import { showEcommerce } from '../lib/flags';
 
-export default function Home() {
+export default async function Home() {
+  const ecommerceEnabled = await showEcommerce();
+
   return (
     <main className="HomePage">
       <VideoHero />
-      <FeaturedProductGrid />
+      {ecommerceEnabled ? <FeaturedProductGrid /> : null}
     </main>
   );
 }
