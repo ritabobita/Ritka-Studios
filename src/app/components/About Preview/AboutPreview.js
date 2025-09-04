27ec59d2beca89dcc759a58dea7e833e56c03436
ritabobitaritabobita <rrosiek97@gmail.com>
2025-09-04 15:43:24 -0400

    content change

--- src/app/components/About Preview/AboutPreview.js
+++ src/app/components/About Preview/AboutPreview.js
@@ -7,7 +7,7 @@ export default function AboutPreview() {
         <h1 className={`${styles.aboutPreviewHeader} text-4xl text-center font-extrabold leading-none tracking-tight md:text-5xl`}>About Ritka Studios</h1>
         <div className={`${styles.AboutContent} flex flex-row items-start justify-center my-12 w-full gap-7`}>
             <p className="md:w-[50rem]">Hi! I'm Rita, the artist behind Ritka Studios. 
-                My ceramics journey began three years ago. I like to say I create pieces that are both fun to look at and fun to use. 
+                My ceramics journey began four years ago. I like to say I create pieces that are both fun to look at and fun to use. 
                 Featuring vibrant colors and experimental glazes that reflect my curiosity in this medium. If you'd like 
                 you can learn more <a href="/about" className="text-blue-500 hover:text-blue-700">here</a>.
               </p>
