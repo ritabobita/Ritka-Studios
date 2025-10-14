dea80fdbec9c452aed72711f63cfa36cbb3492cf
ritabobitaritabobita <rrosiek97@gmail.com>
2025-10-14 15:02:03 -0400

    update events

--- src/app/components/Events Gallery/eventsData.js
+++ src/app/components/Events Gallery/eventsData.js
@@ -4,17 +4,6 @@
 export const EVENTS_DATA = [
   { 
     id: 1, 
-    title: 'Riverbend District Festival', 
-    date: '2025-09-13', 
-    startTime: '12:00', 
-    endTime: '17:00', 
-    timezone: 'America/New_York',
-    location: 'The Riverbend Park - Harrison, NJ',
-    img: '/images/Riverbend-Festival2025.png',
-    url: 'https://mainstreetpops.com/?event=riverbend-district-block-party-artisan-market-2'
-  },
-  { 
-    id: 2, 
     title: 'Tenafly Fall Festival', 
     date: '2025-10-19', 
     startTime: '12:00', 
@@ -25,7 +14,7 @@ export const EVENTS_DATA = [
     url: 'https://mainstreetpops.com/?event=tenafly-fall-fest-2'
   },
   { 
-    id: 3, 
+    id: 2, 
     title: 'Montclair Flea Holiday Market', 
     date: '2025-11-29', 
     startTime: '11:00', 
@@ -36,63 +25,7 @@ export const EVENTS_DATA = [
     url: 'https://www.montclairflea.com/events'
   },
   { 
-    id: 4, 
-    title: 'Montclair Flea Holiday Market', 
-    date: '2025-12-20', 
-    startTime: '11:00', 
-    endTime: '18:00',
-    timezone: 'America/New_York',
-    location: 'Lackawanna Plaza - Montclair, NJ',
-    img: '/images/Montclair-Flea-Holiday-2025.jpeg',
-    url: 'https://www.montclairflea.com/events'
-  }
-  ,
-  { 
-    id: 5, 
-    title: 'Montclair Flea Holiday Market', 
-    date: '2025-12-20', 
-    startTime: '11:00', 
-    endTime: '18:00',
-    timezone: 'America/New_York',
-    location: 'Lackawanna Plaza - Montclair, NJ',
-    img: '/images/Montclair-Flea-Holiday-2025.jpeg',
-    url: 'https://www.montclairflea.com/events'
-  },
-  { 
-    id: 6, 
-    title: 'Montclair Flea Holiday Market', 
-    date: '2025-12-20', 
-    startTime: '11:00', 
-    endTime: '18:00',
-    timezone: 'America/New_York',
-    location: 'Lackawanna Plaza - Montclair, NJ',
-    img: '/images/Montclair-Flea-Holiday-2025.jpeg',
-    url: 'https://www.montclairflea.com/events'
-  },
-  { 
-    id: 7, 
-    title: 'Montclair Flea Holiday Market', 
-    date: '2025-12-20', 
-    startTime: '11:00', 
-    endTime: '18:00',
-    timezone: 'America/New_York',
-    location: 'Lackawanna Plaza - Montclair, NJ',
-    img: '/images/Montclair-Flea-Holiday-2025.jpeg',
-    url: 'https://www.montclairflea.com/events'
-  },
-  { 
-    id: 8, 
-    title: 'Montclair Flea Holiday Market', 
-    date: '2025-12-20', 
-    startTime: '11:00', 
-    endTime: '18:00',
-    timezone: 'America/New_York',
-    location: 'Lackawanna Plaza - Montclair, NJ',
-    img: '/images/Montclair-Flea-Holiday-2025.jpeg',
-    url: 'https://www.montclairflea.com/events'
-  },
-  { 
-    id: 9, 
+    id: 3, 
     title: 'Montclair Flea Holiday Market', 
     date: '2025-12-20', 
     startTime: '11:00', 
